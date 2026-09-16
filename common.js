e54ef18946ac086f149f75501854717a4c1826ae
rohithshinde <rohit.h.shinde1997@gmail.com>
2026-09-16 15:38:48 +0530

    Add files via upload

--- common.js
+++ common.js
@@ -50,6 +50,32 @@
 
 
 /* =========================================================
+   MICROSOFT CLARITY
+========================================================= */
+
+(function () {
+
+    const CLARITY_PROJECT_ID = "yj5jes6asp";
+
+    (function (c, l, a, r, i, t, y) {
+
+        c[a] = c[a] || function () {
+            (c[a].q = c[a].q || []).push(arguments);
+        };
+
+        t = l.createElement(r);
+        t.async = 1;
+        t.src = "https://www.clarity.ms/tag/" + i;
+
+        y = l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t, y);
+
+    })(window, document, "clarity", "script", CLARITY_PROJECT_ID);
+
+})();
+
+
+/* =========================================================
    NAVBAR SCROLL EFFECT
 ========================================================= */
 
